952720416a1dd1894e855effad819b9648e68397
vnframe <vnframe95@icloud.com>
2017-09-21 12:22:54 -0400

    updates (sorta) dynamically

--- app.rb
+++ app.rb
@@ -24,3 +24,25 @@ post "/index" do
     email = params[:user_email]
     db.exec("INSERT INTO phone_book_data(first_name, last_name, street_address, city_state_zip, phone_number, email) VALUES('#{fname}', '#{lname}', '#{street_address}', '#{place}', '#{phone_num}', '#{email}')");
 end
+post '/update' do
+    
+       new_data = params[:new_data]
+       old_data = params[:old_data]
+       column = params[:table_column]
+    
+       case column
+       when 'col_first_name'
+           db.exec("UPDATE phone_book_data SET first_name = '#{new_data}' WHERE first_name = '#{old_data}' ");
+       when 'col_last_name'
+           db.exec("UPDATE phone_book_data SET last_name = '#{new_data}' WHERE last_name = '#{old_data}' ");
+       when 'col_address'
+           db.exec("UPDATE phone_book_data SET street_address = '#{new_data}' WHERE street_address = '#{old_data}' ");
+       when 'col_city_state_zip'
+           db.exec("UPDATE phone_book_data SET city_state_zip = '#{new_data}' WHERE city_state_zip = '#{old_data}' ");
+       when 'col_cell'
+           db.exec("UPDATE phone_book_data SET phone_number = '#{new_data}' WHERE phone = '#{old_data}' ");
+       when 'col_home'
+        db.exec("UPDATE phone_book_data SET email = '#{new_data}' WHERE email = '#{old_data}' ");
+        end
+       redirect '/'
+    end
